b7c672bb648735e469a0fbc51bb0566526879b03
Michael Waldman <mwaldman130@gmail.com>
2020-11-11 16:26:06 -0500

    All dynamic loading now, and added form option for email as well as the endpoint to handle receiving it on the server

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,8 +1,9 @@
 # pages/urls.py
 from django.urls import path 
 from django.views.generic import TemplateView
-from .views import homePageView, RenderedTaskView
+from .views import homePageView, RenderedTaskView, emailView
 urlpatterns = [
     path('hm', RenderedTaskView.as_view(), name='/'),
     path('home', homePageView, name='home'),
+    path('email', emailView, name='home'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse
 from .textparser import TaskTextParser
 from json2html import *
 from django.views.generic import TemplateView
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+
 d = os.getcwd()
 FILE = open(d+"/tasks/chat_files/meeting_saved_chat.txt", "r")
 
@@ -24,5 +28,19 @@ def homePageView(request):
         file.writelines(lines)
     return HttpResponse(tData)
 
+def emailView(request):
+    t = TaskTextParser(FILE)
+    tasks = t.getTasks()
+    tData = json.dumps(tasks)
+    user_email = request.GET.get('email')
+    print(user_email)
+    html_msg = json2html.convert(json = tData)
+    plain_msg = strip_tags(html_msg)
+    from_email='zoomtasks@zoombot.org'
+
+    
+    send_mail('Your Zoom Task List', plain_msg, from_email, [user_email], html_message=html_msg)
+
+    return HttpResponse(request.GET.get('email'))
 class RenderedTaskView(TemplateView):
     template_name = 'home.html'
