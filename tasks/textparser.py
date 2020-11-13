d5651704b211179b7f78f1116c33e0ec935b15eb
Itay Golan <itaygolan19@gmail.com>
2020-11-12 18:30:09 -0600

    add task removal functionality

--- tasks/textparser.py
+++ tasks/textparser.py
@@ -12,7 +12,13 @@ class TaskTextParser:
             if '#' not in message:
                 continue
             task, person = message.split('#')
-            self.nodes[person.strip()].append(task.strip()) # strip whitespaces from messages
+            task, person = task.strip(), person.strip()
+            person_list = person.split(" ")
+            if len(person_list) > 1 and person_list[1] == '--remove':
+                if task in self.nodes[person_list[0]]:
+                    self.nodes[person_list[0]].remove(task)
+            else:
+                self.nodes[person.strip()].append(task.strip()) # strip whitespaces from messages
 
     def getTasks(self):
         self._parse()
